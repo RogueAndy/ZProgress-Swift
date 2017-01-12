fab5172eab1ce84bdacfa92a4fb8f67b4f757d99
dazhongge <zlj_1969_love@126.com>
2017-01-12 17:43:41 +0800

    修改

--- ZProgress-Swift/ZProgress-Swift/ViewControllers/ZProgressViewController.swift
+++ ZProgress-Swift/ZProgress-Swift/ViewControllers/ZProgressViewController.swift
@@ -13,23 +13,8 @@ class ZProgressViewController: ZSBaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let z = ZProgressView.init(frame: CGRect.init(x: 0, y: 0, width: 200, height: 200), circleFrame: CGRect.init(x: 0, y: 0, width: 200, height: 200), strokeColor: .orange, animationType: .ZSlow)
+        print(z)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- ZProgress-Swift/ZProgress-Swift/Views/ZProgressView.swift
+++ ZProgress-Swift/ZProgress-Swift/Views/ZProgressView.swift
@@ -7,15 +7,117 @@
 //
 
 import UIKit
+import Foundation
+
+enum ZAnimationType {
+
+    case ZQuickly
+    case ZCommon
+    case ZSlow
+    
+}
 
 class ZProgressView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    public var animationType: ZAnimationType?
+    public var strokeColor: UIColor?
+    public var circleFrame: CGRect! {
+    
+        willSet(frame) {
+         
+            self.circleFrame = frame
+            self.loadLayout()
+        
+        }
+        
     }
-    */
+    public var startAnimation: Bool? {
+    
+        willSet(animation) {
+        
+            self.startAnimation = animation
+            if self.startAnimation == true {
+            
+                self.animationTimer?.invalidate()
+                self.animationTimer = nil
+                self.shapeLayer.strokeStart = CGFloat(self.strokeStart)
+                self.countNumber = self.strokeStart
+                self.animationTimer = Timer.scheduledTimer(timeInterval: Double(self.timerSecond), target: self, selector: #selector(startAction), userInfo: nil, repeats: true)
+                
+            }
+        
+        }
+        
+    }
+    public var clearAnimation: Bool?
+    public var strokeStart: Float = 0
+    public var strokeEnd: Float = 1
+    
+    private var shapeLayer: CAShapeLayer!
+    private var animationTimer: Timer?
+    private var countNumber: Float!
+    private var timerSecond: Float!
+    private var strokeProgressCount: Float!
+    
+    public init(frame: CGRect, circleFrame: CGRect, strokeColor: UIColor, animationType: ZAnimationType) {
+    
+        super.init(frame: frame)
+        self.animationType = animationType
+        self.circleFrame = circleFrame;
+        self.strokeColor = strokeColor;
+        
+        self.loadInit()
+        self.loadViews()
+        self.loadLayout()
+    }
+    
+    public override init(frame: CGRect) {
+        
+        super.init(frame: frame)
+        self.loadInit()
+        self.loadViews()
+        self.loadLayout()
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func loadInit() {
+    
+        
+        
+    }
+    
+    private func loadViews() {
+    
+    
+    }
+    
+    private func loadLayout() {
+    
+        
+    
+    }
+    
+    
+    // MARK - 
+    
+    @objc private func startAction(timer: Timer) {
+    
+        if self.countNumber >= self.strokeEnd {
+        
+            self.shapeLayer.strokeEnd = CGFloat(self.strokeEnd)
+            self.animationTimer?.invalidate()
+            self.animationTimer = nil
+            return
+        
+        }
+        
+        self.shapeLayer.strokeEnd = CGFloat(self.countNumber)
+        self.countNumber = self.strokeProgressCount + self.countNumber
+    
+    }
 
 }
