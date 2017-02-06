a11c7cefa01cfb02b2c50e2769da0189bdfd7d8a
Rogue Andy <zlj_1969_love@126.com>
2017-02-06 17:05:19 +0800

    胡烈

--- ZProgress-Swift/ZProgress-Swift/Views/ZProgressView.swift
+++ ZProgress-Swift/ZProgress-Swift/Views/ZProgressView.swift
@@ -92,17 +92,37 @@ class ZProgressView: UIView {
     
     private func loadViews() {
     
+        self.shapeLayer = CAShapeLayer()
+        self.shapeLayer.fillColor = UIColor.clear.cgColor
+        self.shapeLayer.lineWidth = 75.0
+        self.shapeLayer.strokeColor = self.strokeColor?.cgColor
+        
+        let path = UIBezierPath.init(ovalIn: self.circleFrame)
+        self.shapeLayer.path = path.cgPath
+        self.layer.addSublayer(self.shapeLayer)
+        self.shapeLayer.strokeStart = 0
+        self.shapeLayer.strokeEnd = 0.0
     
     }
     
     private func loadLayout() {
     
-        
+        self.shapeLayer.frame = self.circleFrame
+        self.shapeLayer.position = CGPoint.init(x: self.frame.size.width / 2.0, y: self.frame.size.height / 2.0)
     
     }
     
+    override func layoutSubviews() {
+        
+        super.layoutSubviews()
+        
+        self.loadLayout()
+        let path = UIBezierPath.init(ovalIn: self.circleFrame)
+        self.shapeLayer.path = path.cgPath
+        
+    }
     
-    // MARK - 
+    // MARK - selector action
     
     @objc private func startAction(timer: Timer) {
     
@@ -117,6 +137,22 @@ class ZProgressView: UIView {
         
         self.shapeLayer.strokeEnd = CGFloat(self.countNumber)
         self.countNumber = self.strokeProgressCount + self.countNumber
+    
+    }
+    
+    @objc private func clearAction(timer: Timer) {
+    
+        if self.countNumber <= self.strokeStart {
+        
+            self.shapeLayer.strokeEnd = CGFloat(self.strokeStart)
+            self.animationTimer?.invalidate()
+            self.animationTimer = nil
+            return
+        
+        }
+        
+        self.shapeLayer.strokeEnd = CGFloat(self.countNumber)
+        self.countNumber = self.countNumber - self.strokeProgressCount
     
     }
 
