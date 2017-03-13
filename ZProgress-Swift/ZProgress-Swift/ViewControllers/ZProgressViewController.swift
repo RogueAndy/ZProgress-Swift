a98a034026308c76123243c6b96f67ddfde81670
Rogue Andy <zlj_1969_love@126.com>
2017-03-13 14:27:46 +0800

    完善 Swift 版本功能

--- ZProgress-Swift/ZProgress-Swift/ViewControllers/ZProgressViewController.swift
+++ ZProgress-Swift/ZProgress-Swift/ViewControllers/ZProgressViewController.swift
@@ -10,11 +10,53 @@ import UIKit
 
 class ZProgressViewController: ZSBaseViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    
+    
+    private var z1: ZProgressView! //= ZProgressView.init(frame: CGRect.init(x: 0, y: 0, width: 200, height: 200), circleFrame: CGRect.init(x: 0, y: 0, width: 200, height: 200), strokeColor: .orange, animationType: .ZSlow)
+    private var change: Bool!
+    
+    override func loadViews() {
+        super.loadViews()
 
-        let z = ZProgressView.init(frame: CGRect.init(x: 0, y: 0, width: 200, height: 200), circleFrame: CGRect.init(x: 0, y: 0, width: 200, height: 200), strokeColor: .orange, animationType: .ZSlow)
-        print(z)
+        self.z1 = ZProgressView.init(frame: CGRect.init(x: 0, y: 0, width: 200, height: 200), circleFrame: CGRect.init(x: 0, y: 0, width: 200, height: 200), strokeColor: .orange, animationType: .ZSlow)
+        
+        self.z1.strokeStart = 0.1
+        self.z1.strokeEnd = 0.9
+        self.view.addSubview(self.z1)
+        
+        let start = UIButton.init(type: .custom)
+        start.addTarget(self, action: #selector(startAction(sender:)), for: .touchUpInside)
+        start.backgroundColor = .orange
+        start.frame = CGRect.init(x: 75, y: 20, width: self.view.frame.size.width - 150, height: 35)
+        self.view.addSubview(start)
+        
+        let clear = UIButton.init(type: .custom)
+        clear.addTarget(self, action: #selector(clearAction(sender:)), for: .touchUpInside)
+        clear.backgroundColor = .red
+        clear.frame = CGRect.init(x: 75, y: 60, width: self.view.frame.size.width - 150, height: 35)
+        self.view.addSubview(clear)
+        
+    }
+    
+    override func loadLayout() {
+        
+        super.loadLayout()
+        self.z1.frame = CGRect.init(x: 0, y: 0, width: 300, height: 300)
+        self.z1.circleFrame = CGRect.init(x: 0, y: 0, width: 300, height: 300)
+        self.z1.center = self.view.center
+        
     }
 
+    @objc private func startAction(sender: UIButton) {
+    
+        self.z1.startAnimation = true
+    
+    }
+    
+    @objc private func clearAction(sender: UIButton) {
+    
+        self.z1.clearAnimation = true
+    
+    }
+    
 }
--- ZProgress-Swift/ZProgress-Swift/Views/ZProgressView.swift
+++ ZProgress-Swift/ZProgress-Swift/Views/ZProgressView.swift
@@ -19,7 +19,26 @@ enum ZAnimationType {
 
 class ZProgressView: UIView {
 
-    public var animationType: ZAnimationType?
+    public var animationType: ZAnimationType! {
+    
+        willSet(type) {
+        
+            self.animationType = type
+            switch self.animationType! {
+            case .ZQuickly:
+                self.strokeProgressCount = 0.03
+                self.timerSecond = 0.005
+            case .ZCommon:
+                self.strokeProgressCount = 0.01
+                self.timerSecond = 0.005
+            case .ZSlow:
+                self.strokeProgressCount = 0.001
+                self.timerSecond = 0.005
+            }
+        
+        }
+    
+    }
     public var strokeColor: UIColor?
     public var circleFrame: CGRect! {
     
@@ -40,7 +59,7 @@ class ZProgressView: UIView {
             
                 self.animationTimer?.invalidate()
                 self.animationTimer = nil
-                self.shapeLayer.strokeStart = CGFloat(self.strokeStart)
+                self.shapeLayer?.strokeStart = CGFloat(self.strokeStart)
                 self.countNumber = self.strokeStart
                 self.animationTimer = Timer.scheduledTimer(timeInterval: Double(self.timerSecond), target: self, selector: #selector(startAction), userInfo: nil, repeats: true)
                 
@@ -49,23 +68,44 @@ class ZProgressView: UIView {
         }
         
     }
-    public var clearAnimation: Bool?
+    public var clearAnimation: Bool? {
+    
+        willSet(animation) {
+        
+            self.clearAnimation = animation
+            if self.clearAnimation == true {
+            
+                self.animationTimer?.invalidate()
+                self.animationTimer = nil
+                self.countNumber = self.strokeEnd
+                self.animationTimer = Timer.scheduledTimer(timeInterval: Double(self.timerSecond), target: self, selector: #selector(clearAction), userInfo: nil, repeats: true)
+                return
+            
+            }
+        
+        }
+    
+    }
     public var strokeStart: Float = 0
     public var strokeEnd: Float = 1
     
-    private var shapeLayer: CAShapeLayer!
+    private var shapeLayer: CAShapeLayer?
     private var animationTimer: Timer?
     private var countNumber: Float!
     private var timerSecond: Float!
     private var strokeProgressCount: Float!
     
+    /// 初始化方法，但是在初始化的时候，所有的 init 方法里，都不会调用私有属性的 willSet 方法，所以把属性赋值放入 loadNeed(frame: CGRect, circleFrame: CGRect, strokeColor: UIColor, animationType: ZAnimationType)，才能让其调用 willSet() 方法
+    ///
+    /// - Parameters:
+    ///   - frame: 设置界面大小
+    ///   - circleFrame: 设置圆圈的界面大小
+    ///   - strokeColor: 设置填充圆圈的颜色
+    ///   - animationType: 动画速度
     public init(frame: CGRect, circleFrame: CGRect, strokeColor: UIColor, animationType: ZAnimationType) {
     
         super.init(frame: frame)
-        self.animationType = animationType
-        self.circleFrame = circleFrame;
-        self.strokeColor = strokeColor;
-        
+        self.loadNeed(frame: frame, circleFrame: circleFrame, strokeColor: strokeColor, animationType: animationType)
         self.loadInit()
         self.loadViews()
         self.loadLayout()
@@ -84,31 +124,40 @@ class ZProgressView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
+    // MARK: - 设置 private 方法
+    
+    private func loadNeed(frame: CGRect, circleFrame: CGRect, strokeColor: UIColor, animationType: ZAnimationType) {
+    
+        self.animationType = animationType
+        self.circleFrame = circleFrame
+        self.strokeColor = strokeColor
+    
+    }
+    
     private func loadInit() {
     
-        
         
     }
     
     private func loadViews() {
     
         self.shapeLayer = CAShapeLayer()
-        self.shapeLayer.fillColor = UIColor.clear.cgColor
-        self.shapeLayer.lineWidth = 75.0
-        self.shapeLayer.strokeColor = self.strokeColor?.cgColor
+        self.shapeLayer!.fillColor = UIColor.clear.cgColor
+        self.shapeLayer!.lineWidth = 75.0
+        self.shapeLayer!.strokeColor = self.strokeColor?.cgColor
         
         let path = UIBezierPath.init(ovalIn: self.circleFrame)
-        self.shapeLayer.path = path.cgPath
-        self.layer.addSublayer(self.shapeLayer)
-        self.shapeLayer.strokeStart = 0
-        self.shapeLayer.strokeEnd = 0.0
+        self.shapeLayer!.path = path.cgPath
+        self.layer.addSublayer(self.shapeLayer!)
+        self.shapeLayer!.strokeStart = 0
+        self.shapeLayer!.strokeEnd = 0.0
     
     }
     
     private func loadLayout() {
     
-        self.shapeLayer.frame = self.circleFrame
-        self.shapeLayer.position = CGPoint.init(x: self.frame.size.width / 2.0, y: self.frame.size.height / 2.0)
+        self.shapeLayer?.frame = self.circleFrame
+        self.shapeLayer?.position = CGPoint.init(x: self.frame.size.width / 2.0, y: self.frame.size.height / 2.0)
     
     }
     
@@ -118,40 +167,46 @@ class ZProgressView: UIView {
         
         self.loadLayout()
         let path = UIBezierPath.init(ovalIn: self.circleFrame)
-        self.shapeLayer.path = path.cgPath
+        self.shapeLayer?.path = path.cgPath
         
     }
     
-    // MARK - selector action
+    // MARK: - selector action
     
+    /// begin current shareLayer
+    ///
+    /// - Parameter timer: time
     @objc private func startAction(timer: Timer) {
     
         if self.countNumber >= self.strokeEnd {
         
-            self.shapeLayer.strokeEnd = CGFloat(self.strokeEnd)
+            self.shapeLayer?.strokeEnd = CGFloat(self.strokeEnd)
             self.animationTimer?.invalidate()
             self.animationTimer = nil
             return
         
         }
         
-        self.shapeLayer.strokeEnd = CGFloat(self.countNumber)
+        self.shapeLayer?.strokeEnd = CGFloat(self.countNumber)
         self.countNumber = self.strokeProgressCount + self.countNumber
     
     }
     
+    /// clear current shareLayer
+    ///
+    /// - Parameter timer: time
     @objc private func clearAction(timer: Timer) {
     
         if self.countNumber <= self.strokeStart {
         
-            self.shapeLayer.strokeEnd = CGFloat(self.strokeStart)
+            self.shapeLayer?.strokeEnd = CGFloat(self.strokeStart)
             self.animationTimer?.invalidate()
             self.animationTimer = nil
             return
         
         }
         
-        self.shapeLayer.strokeEnd = CGFloat(self.countNumber)
+        self.shapeLayer?.strokeEnd = CGFloat(self.countNumber)
         self.countNumber = self.countNumber - self.strokeProgressCount
     
     }
